c14dcce1aec5d8d30037271c03f7bcd7dee43a40
Camilo Victoria <camiloavict@gmail.com>
2020-10-31 00:52:20 -0500

    lazy charge

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import {ImgWrapper, Button, Image} from './styles'
+import React, {useEffect, useRef, useState, Fragment} from 'react'
+import {ImgWrapper, Button, Image, Article} from './styles'
 import {MdFavoriteBorder} from 'react-icons/md'
 
 
 export const PhotoCard = ({id,likes=0,src}) => {
+    
+    const ref = useRef(null);
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) =>{
+            const  {isIntersecting} = entries[0];
+            if(isIntersecting){
+                setShow(true);
+                observer.disconnect();
+            }
+        });
+        observer.observe(ref.current)
+    }, [ref])
+
     return (
-        <article>
+        <Article ref = {ref}>
+        {show && 
+        <Fragment>
             <a href = {`/detail/${id}`}>
                 <ImgWrapper>
                     <Image src = {src}/>
@@ -15,7 +32,9 @@ export const PhotoCard = ({id,likes=0,src}) => {
             <Button>
                 <MdFavoriteBorder size = '32px'/>{likes} Likes!
             </Button>
-        </article>
+        </Fragment>
+        }
+        </Article>
     )
 }
 
