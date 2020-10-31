d37e1b1273de6b7514381dab413b029a83b83e28
Camilo Victoria <camiloavict@gmail.com>
2020-10-31 02:19:00 -0500

    intersection observer done

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,39 +1,46 @@
-import React, {useEffect, useRef, useState, Fragment} from 'react'
-import {ImgWrapper, Button, Image, Article} from './styles'
-import {MdFavoriteBorder} from 'react-icons/md'
+import React, { useState, useRef, Fragment, useEffect } from 'react'
+import { ImgWrapper, Button, Image, Article } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
 
 
-export const PhotoCard = ({id,likes=0,src}) => {
-    
-    const ref = useRef(null);
+export const PhotoCard = ({ id, likes = 0, src }) => {
     const [show, setShow] = useState(false);
+    const ref = useRef(null);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) =>{
-            const  {isIntersecting} = entries[0];
-            if(isIntersecting){
-                setShow(true);
-                observer.disconnect();
-            }
-        });
-        observer.observe(ref.current)
+    useEffect( () => {
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+        ).then(() => {
+            const observer = new window.IntersectionObserver(function (entries) {
+                const { isIntersecting } = entries[0]
+                if (isIntersecting) {
+                    setShow(true)
+                    observer.disconnect()
+                }
+            })
+            observer.observe(ref.current)
+        })
     }, [ref])
 
-    return (
-        <Article ref = {ref}>
-        {show && 
-        <Fragment>
-            <a href = {`/detail/${id}`}>
-                <ImgWrapper>
-                    <Image src = {src}/>
-                </ImgWrapper>
-            </a>
 
-            <Button>
-                <MdFavoriteBorder size = '32px'/>{likes} Likes!
-            </Button>
-        </Fragment>
-        }
+
+    return (
+        <Article ref={ref}>
+            {show &&
+                <Fragment>
+                    <a href={`/detail/${id}`}>
+                        <ImgWrapper>
+                            <Image src={src} />
+                        </ImgWrapper>
+                    </a>
+
+                    <Button>
+                        <MdFavoriteBorder size='32px' />{likes} Likes!
+                </Button>
+                </Fragment>
+            }
         </Article>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const babelRules = [{
   exclude: /node_modules/,
   use: {
     loader: 'babel-loader',
+    plugin: ['@babel/plugin-syntax-dynamic-import'],
     options: {
       presets: ['@babel/preset-env', '@babel/preset-react']
     }
