4cb368c0f671dd604b771b411fcdf9f44345adf7
Camilo Victoria <camiloavict@gmail.com>
2020-10-30 23:27:36 -0500

    cleaning hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,27 +1,49 @@
-import React, {useState, useEffect} from 'react'
-import {Category} from '../Category/index'
-import {List, Item} from './styles'
+import React, { Fragment, useState, useEffect } from 'react'
+import { Category } from '../Category/index'
+import { List, Item } from './styles'
 
 
 export const CategoryList = () => {
 
-  const [categories, setCategories] = useState([]); 
-  
-  useEffect(() =>{
+  const [categories, setCategories] = useState([]);
+  const [showCategorie, setShowCategorie] = useState(false)
+
+  useEffect(() => {
     fetch('https://petgram-camilo.camilovict.vercel.app/categories')
-    .then(res => res.json())
-    .then(response => {
-      setCategories(response);
-    })
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response);
+      })
   }, [])
 
-    return (
-      <List>
-          {
-            categories.map(category => <Item key = {category.id}> <Category {...category}/> </Item>)
-          }
-      </List>
-    )
+  useEffect(() => {
+    const onScroll = (e) => {
+      const position = window.scrollY > 200
+
+      showCategorie !== position && setShowCategorie(position);
+
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  },[showCategorie])
+
+
+
+  const renderList = (fixed) => (<List className={fixed ? 'fixed' : ''}>
+    {
+      categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)
+    }
+  </List>
+  )
+
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showCategorie && renderList(true)}
+    </Fragment>
+  )
 }
 
 
